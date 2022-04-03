9ee58b058674119fc1881773b0fbe9eab9b5787d
Kyeongrok Kim <oceanfog1@gmail.com>
2022-04-03 16:48:44 +0900

    add 설명

--- config.py
+++ config.py
@@ -1,1 +1,2 @@
+# ex) SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://admin:<pw>@<host_name>:3306/<db_name>'
 SQLALCHEMY_DATABASE_URI = 'sqlite:///hello.db'
--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from sqlalchemy import create_engine
 from sqlalchemy_utils import database_exists, create_database
 from config import SQLALCHEMY_DATABASE_URI
 
+# db가 없다면 db를 생성 합니다.
 engine = create_engine(SQLALCHEMY_DATABASE_URI)
+print('Uri:', engine.url)
 if not database_exists(engine.url):
     create_database(engine.url)
 
